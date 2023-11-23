ba3eb3bb702fe39e6d636022536857396c55c6fc
RobbeHeirman <Robbe.Heirman@iqgeo.com>
2023-11-23 16:48:49 +0100

    got something working

--- src/integratedexercise/ingest.py
+++ src/integratedexercise/ingest.py
@@ -4,13 +4,18 @@ import logging
 import os
 import sys
 
+import boto3
+
 import rest_interface
 
 BASE_DIR = "."
 
 
-def write_json_to_s3(s3_bucket, key: str):
-    pass
+def write_json_to_s3(s3_bucket: str, filename: str, json_data: dict):
+    s3 = boto3.resource('s3')
+    b_name = 'data-track-integrated-exercise'
+    s3_object = s3.Object(b_name, filename)
+    s3_object.put(Body=(bytes(json.dumps(json_data).encode('UTF-8'))))
 
 
 def write_json_to_file(subdir: str, filename: str, json_data: dict) -> None:
@@ -21,33 +26,45 @@ def write_json_to_file(subdir: str, filename: str, json_data: dict) -> None:
         f.write(json.dumps(json_data, indent=2))
 
 
-def write_stations_to_files(stations: dict):
+def handle_stations(iso_date: str):
+    print("start fetch stations")
+    stations_endpoint = rest_interface.RestInterface("stations?expanded=true")
+    stations = stations_endpoint.get_list()
+    print("end fetch stations")
+    print("start write stations")
     for station in stations:
         station_id = station["properties"]["id"]
-        filename = f"station_{station_id}.json"
-        write_json_to_file("stations", filename, station)
+        filename = f"robbe-data/{iso_date}/stations/station_{station_id}.json"
+        write_json_to_s3("", filename, station)
+    print("end fetch stations")
 
 
 def handle_timeseries(iso_date: str):
     """"
     Require date in iso_date
     """
+
+    print("start fetch timeseries keys")
     timespan = f"PT24H/{iso_date}"
     timeseries = rest_interface.RestInterface(f"timeseries?timespan={timespan}").get_list()
-    timeserie_data = rest_interface.TimeSeriesRestInterface("timeseries").get_timeseries_data_of_keys(timeseries, timespan)
-
+    print("end fetch timeseries keys")
+    print("start fetch timeseries data")
+    timeserie_data = rest_interface.TimeSeriesRestInterface("timeseries").get_timeseries_data_of_keys(timeseries,
+                                                                                                      timespan)
+    print("end fetch time series data")
+    print("start write time series")
     for timeserie in timeseries:
         timeserie_id = timeserie['id']
         timeserie['values'] = timeserie_data[timeserie_id]["values"]
-        filename = f"timeseries_{timeserie_id}.json"
-        write_json_to_file(f"{iso_date}/timeseries", filename, timeserie)
+        filename = f"robbe-data/{iso_date}/timeseries/timeseries_{timeserie_id}.json"
+        write_json_to_s3("", filename, timeserie)
+    print("end write time series")
 
 
 def process_raw_data(s3_bucket: str, date: str):
     # TODO handle query params gracefully
-    stations_endpoint = rest_interface.RestInterface("stations?expanded=true")
-    stations = stations_endpoint.get_list()
-    write_stations_to_files(stations)
+    handle_stations(date)
+    handle_timeseries(date)
 
 
 def main():
@@ -67,5 +84,5 @@ def main():
 if __name__ == "__main__":
     # TODO : fix this
     # process_raw_data("", "")
-    handle_timeseries("2023-11-22")
+    process_raw_data("", "2023-11-22")
     # main()
