a8763baac02c53dcf6c0b995d5c82e44ecbe7a18
Robbe Heirman <robbe.heirman@student.uantwerpen.be>
2023-12-12 19:20:22 +0000

    dag

--- dags/integrated_exercise.py
+++ dags/integrated_exercise.py
@@ -1,14 +1,25 @@
 from airflow import DAG
 from airflow.providers.amazon.aws.operators.batch import BatchOperator
+import datetime as dt
 
 dag = DAG(
-
+    dag_id="robbe-dag",
+    description="Weather data from API to S3.",
+    default_args={"owner": "Robbe Heirman"},
+    schedule_interval="@daily",
+    start_date=dt.datetime(2023, 12, 1),
 )
 
-ingest = BatchOperator(
-    task_id="ingest",
-    job_name="robbe-datatrack-ingest",
-    job_queue="integrated-exercise-job-queue",
-    job_definition="robbe-datatrack-2",
-    overrides=JOB_OVERRIDES,
-)
+with dag:
+    ingest = BatchOperator(
+        task_id="robbe-datatrack-ingest",
+        job_name="robbe-datatrack-ingest",
+        job_queue="integrated-exercise-job-queue",
+        job_definition="robbe-datatrack-2",
+        overrides={
+            'command': [
+                "python", "/src/integratedexercise/ingest.py",
+                "-d","{{ ds }}",
+                "-e","data-track-integrated-exercise"
+            ]}
+    )
